9963c5fa824715801ae5016cad3e6ba46ece6428
JoshDavid <31487826+JoshDavid@users.noreply.github.com>
2017-11-28 20:08:55 -0500

    Add files via upload

---
+++ header.php
@@ -0,0 +1,14 @@
+<!--
+    Include this header file using the include_once php command.
+    It will place a the navigation bar in every file it is called in. 
+    If the header file needs to change, changing it once in here is sufficient
+    for all the other pages to update
+-->
+
+
+<ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="courses.php">Courses</a></li>
+  <li><a href="MorseDecoder.php">Morse Translator</a></li>
+  <li><a href="#about">Sandbox</a></li>
+</ul>
